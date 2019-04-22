cac8d7bc3e19f1efbff1c2e451a9399b867aa0b8
andyafene <andy.afene-mebaley@epitech.eu>
2019-04-22 15:37:28 +0200

    Creating to the database with PHP

--- Demo/index.php
+++ Demo/index.php
@@ -1,3 +1,13 @@
+<?php
+$con = mysqli_connect("localhost", "root", "", "smallworld");
+
+if(mysqli_connect_errno()) {
+	echo "Failed to connect: ". mysqli_connect_errno();
+}
+
+$query = mysqli_query($con, "INSERT INTO test VALUES(NULL, 'toto')");
+?>
+
 <html>
 <head>
 	<title>My test</title>
