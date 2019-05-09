489171e7b204ba145617891232cb9b681b00abe9
andyafene <andy.afene-mebaley@epitech.eu>
2019-05-09 14:33:58 +0200

    Adding Twitter Bootstrap

--- Demo/includes/header.php
+++ Demo/includes/header.php
@@ -12,6 +12,9 @@ else{
 
 <html>
 <head>
-	<title>My test</title>
+	<title>Welcome to Small World</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+	<script src="assets/js/bootstrap.js"></script>
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
 </head>
 <body>
