9a99c67b623c667b14061e42aef6cf9ab4059da3
andyafene <andy.afene-mebaley@epitech.eu>
2019-05-12 19:44:40 +0200

    Creating the Blue Top Bar

--- Demo/includes/header.php
+++ Demo/includes/header.php
@@ -13,8 +13,17 @@ else{
 <html>
 <head>
 	<title>Welcome to Small World</title>
+	<!--JavaScripte-->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 	<script src="assets/js/bootstrap.js"></script>
+	<!--CSS-->	
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
+
+<div class="top_bar">
+	<div class="logo">
+		<a href="index.php">Small World</a>		
+	</div>	
+</div>
