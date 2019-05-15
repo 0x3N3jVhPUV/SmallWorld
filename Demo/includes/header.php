0b4d7b22c42e040114948ae34c0460cd7762e26d
andyafene <andy.afene-mebaley@epitech.eu>
2019-05-15 09:51:01 +0200

    Navigation Bar Icons

--- Demo/includes/header.php
+++ Demo/includes/header.php
@@ -3,6 +3,8 @@ require 'config/config.php';
 
 if (isset($_SESSION['username'])) {
 	$userLoggIn = $_SESSION['username'];
+	$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggIn'");
+	$user = mysqli_fetch_array($user_details_query);
 }
 else{
 	header("Location: register.php");
@@ -16,7 +18,8 @@ else{
 	<!--JavaScripte-->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 	<script src="assets/js/bootstrap.js"></script>
-	<!--CSS-->	
+	<!--CSS-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
@@ -25,5 +28,25 @@ else{
 <div class="top_bar">
 	<div class="logo">
 		<a href="index.php">Small World</a>		
-	</div>	
+	</div>
+	<nav>
+		<a href="#">
+			<?php echo $user['first_name']; ?>
+		</a>
+		<a href="index.php">
+			<i class="fas fa-home"></i>
+		</a>
+		<a href="#">
+			<i class="fas fa-envelope"></i>
+		</a>
+		<a href="#">
+			<i class="fas fa-bell"></i>
+		</a>
+		<a href="#">
+			<i class="fas fa-users"></i>
+		</a>
+		<a href="#">
+			<i class="fas fa-cog"></i>
+		</a>
+	</nav>	
 </div>
