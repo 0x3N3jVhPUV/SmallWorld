26a9925976f1afcfc0a41d41805f77193b84fddf
andyafene <andy.afene-mebaley@epitech.eu>
2019-05-22 16:06:41 +0200

    Creating the Newsfeed Column - Part 1

--- Demo/index.php
+++ Demo/index.php
@@ -17,6 +17,13 @@ session_destroy();
 			 ?>
 		</div>		 
 	</div>
+	<div class="main_column column">
+		<form class="post_form" action="index.php" method="POST">
+			<textarea name="post_text" id="post_text" placeholder="Got something to say?"></textarea>
+			<input type="submit" name="post" id="post_button" value="Post">
+			<br>			
+		</form>		
+	</div>
 </div>	
 </body>
 </html>
