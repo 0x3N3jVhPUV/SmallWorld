774a1a5146b68f33574cec55a20c6508ea63d2f1
andyafene <andy.afene-mebaley@epitech.eu>
2019-05-22 11:51:42 +0200

    Adding Data to the User Details Panel

--- Demo/index.php
+++ Demo/index.php
@@ -4,7 +4,18 @@ include("includes/header.php");
 session_destroy();
 ?>
 	<div class="user_details column">
-		<a href="#"> <img src="<?php echo $user{'profile_pic'};?>"></a>		
+		<a href="#"> <img src="<?php echo $user{'profile_pic'};?>"></a>
+		<div class="user_details_left_right">
+			<a href="">
+				<?php 
+					echo $user['first_name'] ." ". $user['last_name']. "<br>";
+				 ?>
+			</a>
+			<?php 
+				echo "Posts: " . $user['num_posts']. "<br>";
+				echo "Likes: " . $user['num_likes'];
+			 ?>
+		</div>		 
 	</div>
 </div>	
 </body>
